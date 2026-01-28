ec717454e93471b7d29bad542074ee359214d006
Benard Gomashie <benardgomashie@gmail.com>
2026-01-28 14:42:09 +0000

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -116,16 +116,7 @@ export default function Home() {
           </motion.div>
         </div>
 
-        {/* Scroll Indicator */}
-        <motion.div
-          className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-        >
-          <div className="w-6 h-10 border-2 border-golden rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-golden rounded-full mt-2"></div>
-          </div>
-        </motion.div>
+        
       </section>
 
       {/* About Quick Intro */}
